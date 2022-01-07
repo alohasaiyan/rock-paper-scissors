b45a8eb3e59a6d184a66b8e95c25bc31477f1b34
Sai Yan <synzgrsck@gmail.com>
2022-01-06 20:13:26 -0400

    Created function computerPlay(n)

---
+++ script.js
@@ -0,0 +1,10 @@
+function computerPlay(n) {
+    switch (n) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
