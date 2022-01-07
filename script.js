a43d00cd53213098a8c69c394550eae2f645b580
Sai Yan <synzgrsck@gmail.com>
2022-01-06 20:32:58 -0400

    Created playRound function

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ function computerPlay(n) {
             return 'scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    
+    const winMsg = `You win! ${playerSelection} beats ${computerSelection}`
+    const loseMsg = `You lose! ${computerSelection} beat ${playerSelection}`
+    const drawMsg = `Draw! You both chose ${playerSelection}`
+
+    if (playerSelection === computerSelection) {
+        return drawMsg;
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return loseMsg;
+        } else if (computerSelection === 'scissors') {
+            return winMsg;
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return loseMsg;
+        } else if (computerSelection === 'rock') {
+            return winMsg;
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return loseMsg;
+        } else if (computerSelection === 'paper') {
+            return winMsg;
+        }
+    }
+}
